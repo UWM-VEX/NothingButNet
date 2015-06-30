596dca78f84230a1ae4c604a44739aabd21a3b63
erikorlowski <erik.orlowski@gmail.com>
2015-06-29 20:27:04 -0500

    Added Utilities functions.

--- SingleFlyWheelShooter/include/main.h
+++ SingleFlyWheelShooter/include/main.h
@@ -44,6 +44,10 @@
 #define MAIN_H_
 
 #include <API.h>
+#include "BangBangController.h"
+#include "PantherMotor.h"
+#include "SingleFlyWheelShooter.h"
+#include "Utilities.h"
 
 // Allow usage of this file in C++ programs
 #ifdef __cplusplus
